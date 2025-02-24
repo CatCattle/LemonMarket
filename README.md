85312bb03ee3f7b1c79eb0697f3a1c3fd8a76f5a
CatCattle <2579853504@qq.com>
2025-02-24 18:07:25 +0800

    sql update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LemonMarket
 This is a srp project of scut, aiming to build a experiment program to simulate the lemon market theory.
+
+运行需要在mysql中创建一个database，名叫lemon_market，并将文件夹中的lemon_market.sql导入到这个database中，具体如何导入可以问ai。
--- lemon_market.sql
+++ lemon_market.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: lemon_market
 -- ------------------------------------------------------
--- Server version	8.0.36
+-- Server version	8.0.40
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,7 +26,7 @@ CREATE TABLE `buyers` (
   `BuyerID` int NOT NULL,
   `DemandPrice` int DEFAULT NULL,
   `BoughtCar` tinyint DEFAULT NULL,
-  `TargetLevel` varchar(1) DEFAULT NULL,
+  `TargetLevel` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`BuyerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -37,36 +37,39 @@ CREATE TABLE `buyers` (
 
 LOCK TABLES `buyers` WRITE;
 /*!40000 ALTER TABLE `buyers` DISABLE KEYS */;
+INSERT INTO `buyers` VALUES (1,100000,0,'66'),(2,200000,0,'67'),(3,200000,0,'67'),(4,100000,0,'65'),(5,100000,0,'65'),(6,400000,0,'66'),(7,200000,0,'66'),(8,200000,0,'67'),(9,100000,0,'67');
 /*!40000 ALTER TABLE `buyers` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `cars_for_sale`
+-- Table structure for table `cars`
 --
 
-DROP TABLE IF EXISTS `cars_for_sale`;
+DROP TABLE IF EXISTS `cars`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cars_for_sale` (
-  `CarID` int NOT NULL,
-  `Level` varchar(1) NOT NULL,
+CREATE TABLE `cars` (
+  `CarID` varchar(16) NOT NULL,
+  `Level` varchar(16) NOT NULL,
   `OriginalPrice` int NOT NULL,
   `Lemon` tinyint NOT NULL,
   `DF` double NOT NULL,
   `CurrentValue` int DEFAULT NULL,
-  `Rounds` int DEFAULT NULL,
-  `AveragePrice` int DEFAULT NULL,
+  `Round` int DEFAULT NULL,
+  `Price` int DEFAULT NULL,
+  `Owner` enum('DEALER','SELLER') DEFAULT 'SELLER',
   PRIMARY KEY (`CarID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `cars_for_sale`
+-- Dumping data for table `cars`
 --
 
-LOCK TABLES `cars_for_sale` WRITE;
-/*!40000 ALTER TABLE `cars_for_sale` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cars_for_sale` ENABLE KEYS */;
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+INSERT INTO `cars` VALUES ('EQ','A',100000,0,0.774,62694,1,0,'DEALER'),('FF','B',200000,0,0.644,104328,1,0,''),('GS','A',100000,0,0.465,37665,1,0,'DEALER'),('HS','A',100000,0,0.654,52974,1,0,''),('IQ','A',100000,0,0.572,46331,1,0,'DEALER'),('JS','A',100000,0,0.672,54432,1,0,'DEALER'),('JX','A',100000,0,0.799,64719,1,0,'DEALER'),('LR','A',100000,0,0.595,48195,1,0,'DEALER'),('MY','B',200000,1,0.544,17625,1,0,''),('NP','A',100000,0,0.796,64476,1,0,'DEALER'),('QU','B',200000,0,0.714,115668,1,0,'DEALER'),('RF','A',100000,0,0.753,60993,1,0,''),('RZ','C',400000,0,0.782,253368,1,0,'DEALER'),('SC','A',100000,0,0.736,59616,1,0,'DEALER'),('SD','A',100000,0,0.708,57348,1,0,''),('ST','C',400000,0,0.617,199908,1,0,''),('SV','B',200000,0,0.484,78408,1,0,'DEALER'),('TX','B',200000,1,0.709,22971,1,0,''),('UJ','A',100000,0,0.666,53946,1,0,'DEALER'),('WN','B',200000,0,0.771,124902,1,0,'DEALER'),('WV','A',100000,1,0.471,7630,1,0,'DEALER'),('XK','C',400000,0,0.656,212544,1,0,''),('ZA','B',200000,0,0.706,114372,1,0,'DEALER'),('ZC','C',400000,0,0.705,228420,1,0,'DEALER'),('ZO','A',100000,0,0.654,52974,1,0,'DEALER'),('ZR','B',200000,0,0.709,114858,1,0,'');
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -91,6 +94,7 @@ CREATE TABLE `dealers` (
 
 LOCK TABLES `dealers` WRITE;
 /*!40000 ALTER TABLE `dealers` DISABLE KEYS */;
+INSERT INTO `dealers` VALUES (1,113040,-386960,4),(2,-192424,-692424,7),(3,-48055,-548055,7);
 /*!40000 ALTER TABLE `dealers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,11 +107,11 @@ DROP TABLE IF EXISTS `inventory`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `inventory` (
   `DealerID` int NOT NULL,
-  `CarID` int NOT NULL,
+  `CarID` varchar(16) NOT NULL,
   PRIMARY KEY (`DealerID`,`CarID`),
-  KEY `CarID` (`CarID`),
-  CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`DealerID`) REFERENCES `dealers` (`DealerID`),
-  CONSTRAINT `inventory_ibfk_2` FOREIGN KEY (`CarID`) REFERENCES `cars_for_sale` (`CarID`)
+  KEY `car_idx` (`CarID`),
+  CONSTRAINT `car` FOREIGN KEY (`CarID`) REFERENCES `cars` (`CarID`),
+  CONSTRAINT `dealer` FOREIGN KEY (`DealerID`) REFERENCES `dealers` (`DealerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -117,6 +121,7 @@ CREATE TABLE `inventory` (
 
 LOCK TABLES `inventory` WRITE;
 /*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES (3,'EQ'),(2,'FF'),(3,'FF'),(2,'GS'),(3,'GS'),(1,'HS'),(3,'HS'),(1,'IQ'),(1,'JS'),(3,'JS'),(2,'JX'),(1,'LR'),(3,'LR'),(1,'MY'),(2,'MY'),(3,'NP'),(2,'QU'),(3,'QU'),(2,'RF'),(3,'RF'),(1,'RZ'),(2,'RZ'),(1,'SC'),(2,'SC'),(2,'SD'),(2,'ST'),(3,'ST'),(2,'SV'),(3,'SV'),(1,'TX'),(3,'TX'),(1,'UJ'),(2,'UJ'),(3,'WN'),(1,'WV'),(2,'WV'),(1,'XK'),(3,'XK'),(2,'ZA'),(3,'ZA'),(1,'ZC'),(2,'ZC'),(2,'ZO'),(3,'ZO'),(2,'ZR'),(3,'ZR');
 /*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -128,11 +133,11 @@ DROP TABLE IF EXISTS `sellers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `sellers` (
-  `SellersID` int NOT NULL,
+  `SellerID` int NOT NULL,
   `SellPrice` int DEFAULT NULL,
   `SoldCar` tinyint DEFAULT NULL,
   `SellCarID` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`SellersID`)
+  PRIMARY KEY (`SellerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -142,6 +147,7 @@ CREATE TABLE `sellers` (
 
 LOCK TABLES `sellers` WRITE;
 /*!40000 ALTER TABLE `sellers` DISABLE KEYS */;
+INSERT INTO `sellers` VALUES (1,0,0,'WV'),(2,0,0,'IQ'),(3,0,0,'RZ'),(4,0,0,'ZO'),(5,0,0,'ZA'),(6,0,0,'NP'),(7,0,0,'QU'),(8,0,0,'GS'),(9,0,0,'SV');
 /*!40000 ALTER TABLE `sellers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -154,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-13 10:52:07
+-- Dump completed on 2025-02-21 17:10:56
